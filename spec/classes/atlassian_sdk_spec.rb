fa8f6abb830f000c30c1edb14df781e2e3e4b93e
Cédric FINANCE <cedric.finance@finandric.com>
2014-03-23 01:44:40 +0100

    Fix File[undef]

--- spec/classes/atlassian_sdk_spec.rb
+++ spec/classes/atlassian_sdk_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe 'atlassian_sdk' do
+  let(:facts) { default_test_facts }
+
   it do
     should contain_homebrew__tap("add atlassian tap to homebrew").with({
       :source => "atlassian/tap"
